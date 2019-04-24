4375d3e3081a3036d716546048bcd549c94eda75
alex-w-dev <yampi.ru@gmail.com>
2019-04-24 21:49:20 +0500

    prepare grass

--- src/js/index.ts
+++ src/js/index.ts
@@ -1,1 +1,31 @@
-alert(0);
+interface GameParams {
+  width: number;
+  height: number;
+  fieldParent?: HTMLElement;
+}
+
+class Game {
+  fieldParent: HTMLElement = document.body;
+  field: HTMLDivElement;
+  fieldWidth: number;
+  fieldHeight: number;
+
+  constructor(gameParams: GameParams) {
+    this.field = document.createElement('div');
+    this.field.style.width = (this.fieldWidth = gameParams.width) + 'px';
+    this.field.style.height = (this.fieldHeight = gameParams.height) + 'px';
+    this.field.style.background = `url(data:image/gif;base64,${getGrassImageBase64()})`;
+
+    this.fieldParent.innerHTML = '';
+    this.fieldParent.appendChild(this.field);
+  }
+}
+
+new Game({
+  width: 400,
+  height: 400,
+});
+
+function getGrassImageBase64() {
+  return '/9j/4AAQSkZJRgABAQEASABIAAD/4QAiRXhpZgAATU0AKgAAAAgAAQESAAMAAAABAAEAAAAAAAD/2wBDAAIBAQIBAQICAgICAgICAwUDAwMDAwYEBAMFBwYHBwcGBwcICQsJCAgKCAcHCg0KCgsMDAwMBwkODw0MDgsMDAz/2wBDAQICAgMDAwYDAwYMCAcIDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAz/wAARCAAPAA8DASIAAhEBAxEB/8QAHwAAAQUBAQEBAQEAAAAAAAAAAAECAwQFBgcICQoL/8QAtRAAAgEDAwIEAwUFBAQAAAF9AQIDAAQRBRIhMUEGE1FhByJxFDKBkaEII0KxwRVS0fAkM2JyggkKFhcYGRolJicoKSo0NTY3ODk6Q0RFRkdISUpTVFVWV1hZWmNkZWZnaGlqc3R1dnd4eXqDhIWGh4iJipKTlJWWl5iZmqKjpKWmp6ipqrKztLW2t7i5usLDxMXGx8jJytLT1NXW19jZ2uHi4+Tl5ufo6erx8vP09fb3+Pn6/8QAHwEAAwEBAQEBAQEBAQAAAAAAAAECAwQFBgcICQoL/8QAtREAAgECBAQDBAcFBAQAAQJ3AAECAxEEBSExBhJBUQdhcRMiMoEIFEKRobHBCSMzUvAVYnLRChYkNOEl8RcYGRomJygpKjU2Nzg5OkNERUZHSElKU1RVVldYWVpjZGVmZ2hpanN0dXZ3eHl6goOEhYaHiImKkpOUlZaXmJmaoqOkpaanqKmqsrO0tba3uLm6wsPExcbHyMnK0tPU1dbX2Nna4uPk5ebn6Onq8vP09fb3+Pn6/9oADAMBAAIRAxEAPwClaiSO0WSGFluIdkaO/Ee4qflB5U4YxncT27iru640uWRUmt5YsbgJrgKhOcE5ZuTyDyMEMMYxUawSW93JI0a/aIwsRhwOr7ZCSM7T1HRgSfbNOtIFsUWaEqscYW3XCFCoKKwbg9wFBGM5Y9ea/kR3ufL8zi7L+u5//9k=';
+}
